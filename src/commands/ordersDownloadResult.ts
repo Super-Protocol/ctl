64462347e936d8e995f1e8c55090602e54bffdfc
Marat Idrisov <43449302+Idris0v@users.noreply.github.com>
2022-08-25 11:37:43 +0300

    SP-1877 CLI. Incorrect result file format (#38)
    * SP-1877 CLI. Incorrect result file format
    
    * added tests and error handling
    
    * added ci checks on pull request

--- src/commands/ordersDownloadResult.ts
+++ src/commands/ordersDownloadResult.ts
@@ -6,10 +6,9 @@ import { isCommandSupported } from "../services/uplinkSetupHelper";
 import path from "path";
 import getOrderResult from "../services/getOrderResult";
 import { Encryption, Resource, ResourceType, StorageProviderResource, UrlResource } from "@super-protocol/dto-js";
-import { Crypto } from "@super-protocol/sdk-js";
+import { Crypto, Config as BlockchainConfig } from "@super-protocol/sdk-js";
 import downloadFileByUrl from "../services/downloadFileByUrl";
 import initBlockchainConnector from "../services/initBlockchainConnector";
-import { Config as BlockchainConfig } from "@super-protocol/sdk-js/build/BlockchainConnector";
 
 export type FilesDownloadParams = {
     blockchainConfig: BlockchainConfig;
@@ -17,6 +16,8 @@ export type FilesDownloadParams = {
     localPath: string;
     resultDecryptionKey: string;
 };
+
+export const localTxtPath = "./result.txt";
 
 export default async (params: FilesDownloadParams): Promise<void> => {
     Printer.print("Connecting to blockchain...");
@@ -29,38 +30,53 @@ export default async (params: FilesDownloadParams): Promise<void> => {
     const encrypted = encryptedResult || encryptedError;
     Printer.print("Order result found, decrypting...");
 
-    let encryptedResultObject: { resource: Encryption; encryption?: Encryption };
-    try {
-        encryptedResultObject = JSON.parse(encrypted);
-    } catch (e) {
-        Printer.print("Unable to parse order result, saving raw result to file...");
-        await fs.writeFile(path.join(process.cwd(), params.localPath), encrypted);
-        Printer.print(`Raw result extracted and saved to ${params.localPath}`);
+    let encryptedResultObject: { resource: Encryption; encryption?: Encryption } = await tryParse(encrypted);
+    if (!encryptedResultObject) {
+        await writeResult(localTxtPath, encrypted, "Unable to parse order result, saving raw result to file...");
         return;
     }
 
     let decrypted: { resource?: Resource; encryption?: Encryption } = {};
-    if (encryptedResultObject.resource) {
-        encryptedResultObject.resource.key = params.resultDecryptionKey;
-        decrypted.resource = JSON.parse(await Crypto.decrypt(encryptedResultObject.resource));
-
-        if (encryptedResultObject.encryption) {
-            encryptedResultObject.encryption.key = params.resultDecryptionKey;
-            decrypted.encryption = JSON.parse(await Crypto.decrypt(encryptedResultObject.encryption));
+    if (encryptedResultObject.resource && encryptedResultObject.encryption) {
+        const decryptedResourceStr = await tryDecrypt(encryptedResultObject.resource, params.resultDecryptionKey);
+        const decryptedEncryptionStr = await tryDecrypt(encryptedResultObject.encryption, params.resultDecryptionKey);
+        if (!decryptedResourceStr || !decryptedEncryptionStr) {
+            await writeResult(
+                localTxtPath,
+                JSON.stringify(encrypted),
+                "Unable to decrypt order result, saving raw result to file..."
+            );
+            return;
+        }
+        decrypted.resource = await tryParse(decryptedResourceStr);
+        decrypted.encryption = await tryParse(decryptedEncryptionStr);
+        if (!decrypted.resource || !decrypted.encryption) {
+            await writeResult(
+                localTxtPath,
+                JSON.stringify({ resource: decryptedResourceStr, encryption: decryptedEncryptionStr }),
+                "Order result decrypted, but was unable to parse, saving raw result to file..."
+            );
+            return;
         }
     } else {
-        const stringEncryptedResult: Encryption = encryptedResultObject as unknown as Encryption;
-        stringEncryptedResult.key = params.resultDecryptionKey;
-        const stringResult = await Crypto.decrypt(stringEncryptedResult);
-
+        const stringResult = await tryDecrypt(
+            encryptedResultObject as unknown as Encryption,
+            params.resultDecryptionKey
+        );
+        if (!stringResult) {
+            await writeResult(
+                localTxtPath,
+                JSON.stringify(encrypted),
+                "Unable to decrypt order result, saving raw result to file..."
+            );
+            return;
+        }
         try {
             const result = JSON.parse(stringResult);
             if (!result.resource) throw Error("Resource not found");
             decrypted = result;
         } catch (e) {
-            Printer.print("Result message decrypted, saving to file...");
-            await fs.writeFile(path.join(process.cwd(), params.localPath), stringResult);
-            Printer.print(`Result message extracted successfully and saved to ${params.localPath}`);
+            await writeResult(localTxtPath, stringResult, "Result message decrypted, saving to file...");
             return;
         }
     }
@@ -83,7 +99,7 @@ export default async (params: FilesDownloadParams): Promise<void> => {
         case ResourceType.StorageProvider:
             if (!isCommandSupported()) return;
 
-            const storageProviderResource: StorageProviderResource = resource as StorageProviderResource;
+            const storageProviderResource = resource as StorageProviderResource;
             await downloadService(
                 storageProviderResource.filepath,
                 localPath,
@@ -112,3 +128,26 @@ export default async (params: FilesDownloadParams): Promise<void> => {
     await fs.unlink(localPath);
     Printer.print("Done");
 };
+
+async function tryDecrypt(encryption: Encryption, decryptionKey: string): Promise<string | undefined> {
+    try {
+        encryption.key = decryptionKey;
+        return await Crypto.decrypt(encryption);
+    } catch (e) {
+        return;
+    }
+}
+
+async function tryParse(text: string): Promise<any> {
+    try {
+        return JSON.parse(text);
+    } catch (e) {
+        return;
+    }
+}
+
+async function writeResult(localPath: string, content: string, message: string) {
+    Printer.print(message);
+    await fs.writeFile(path.join(process.cwd(), localPath), content);
+    Printer.print(`Result successfully saved to ${localPath}`);
+}
