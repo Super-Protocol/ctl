c841e6cea743910c3dfaa797c68943f46169c840
Vladyslav Marchuk <134324829+marchuk-vlad@users.noreply.github.com>
2024-02-01 18:59:00 +0300

    Fix base64 regex check (#219)

--- src/constants.ts
+++ src/constants.ts
@@ -10,8 +10,7 @@ export const DEFAULT_PCCS_SERVICE = 'https://pccs.superprotocol.io';
 export const BACKEND_URL_DEFAULT = 'https://bff.testnet.superprotocol.com/graphql';
 export const BLOCKCHAIN_RPC_URL_DEFAULT = 'https://mumbai.polygon.superprotocol.com/hesoyam';
 export const SMART_CONTRACT_ADDRESS_DEFAULT = '0xA7Ff565f26b93926e4e6465Eb81d48EfF456848b';
-export const BASE64_CHECK_REGEX =
-  /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}(?:==)?|[A-Za-z0-9+/]{3}=?|[A-Za-z0-9+/]{4})$/;
+export const BASE64_CHECK_REGEX = /^[A-Za-z0-9+/]{1,}={0,3}$/;
 export const JWT_CHECK_REGEX = /(^[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$)/;
 export const CONFIG_DEFAULT_FILENAME = './config.json';
 export const DEFAULT_TEE_AMOUNT_FOR_APPROVE = 1e10;
