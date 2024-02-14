d6dcd6a5f768afca9fa0ba22dda4c8413811dad5
Dmitry Somov <smv.dmitry@gmail.com>
2024-02-14 16:18:20 +0300

    SP-3827 Update isCommandSupported

--- src/services/uplinkSetupHelper.ts
+++ src/services/uplinkSetupHelper.ts
@@ -1,8 +1,8 @@
 import Printer from '../printer';
 
-const SUPPORTED_PLATFORMS: NodeJS.Platform[] = ['darwin', 'linux'];
+const SUPPORTED_PLATFORMS: NodeJS.Platform[] = ['darwin', 'linux', 'win32'];
 
-export const isCommandSupported = () => {
+export const isCommandSupported = (): boolean => {
   if (!SUPPORTED_PLATFORMS.includes(process.platform)) {
     Printer.print(
       `Command is not supported on this platform, list of supported platforms: ${SUPPORTED_PLATFORMS}`,
