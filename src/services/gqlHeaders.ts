cf3214de025e90df2b83f1b5c10af5f9c0f47fc5
serega-k <zloebuch@gmail.com>
2024-09-16 13:45:33 +0300

    Revert "feat(gqlHeaders): tmp - stub"
    This reverts commit ff6841c8497f01b0c29da6dd09dc2ac2fb844373.

--- src/services/gqlHeaders.ts
+++ src/services/gqlHeaders.ts
@@ -1,9 +1,9 @@
 // @ts-ignore
+import packageJson from '../../package.json' assert { type: 'json' };
 
 export default (accessToken: string): { [header: string]: string } => {
   return {
     Authorization: `Bearer ${accessToken}`,
-    // 'X-Sdk-Version': packageJson.dependencies['@super-protocol/sdk-js'],
-    'X-Sdk-Version': '2.2.0-beta.79',
+    'X-Sdk-Version': packageJson.dependencies['@super-protocol/sdk-js'],
   };
 };
